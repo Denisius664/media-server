13453ed0d8a5b4aa742550041e1e4073e5800bbb
Denis <denismir423@gmail.com>
2020-07-15 21:32:38 +1100

    updates for home

--- src/main/java/inc/sneid/media_server/HomeController.java
+++ src/main/java/inc/sneid/media_server/HomeController.java
@@ -1,13 +1,27 @@
 package inc.sneid.media_server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
 
+    @Value("${path.image}")
+    private String pathImage;
+
     @GetMapping
-    public String home(){
+    public String home(Model model){
+        File[] images = new File(pathImage).listFiles();
+        if (images != null) {
+            model.addAttribute("files",
+                    Arrays.stream(images).map(File::getName).collect(Collectors.toList()));
+        }
         return "home";
     }
 
